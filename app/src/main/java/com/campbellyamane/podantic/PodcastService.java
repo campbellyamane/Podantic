67a92788424f003d1d6b9b387da288f9e6743b71
hathair5 <campbellyamane@gmail.com>
2018-06-10 14:21:23 -0700

    Built Notification

--- app/src/main/java/com/campbellyamane/podantic/PodcastService.java
+++ app/src/main/java/com/campbellyamane/podantic/PodcastService.java
@@ -1,15 +1,29 @@
 package com.campbellyamane.podantic;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.AudioManager;
+import android.media.MediaMetadata;
 import android.media.MediaPlayer;
+import android.media.session.MediaController;
+import android.media.session.MediaSession;
+import android.media.session.MediaSessionManager;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.RemoteException;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class PodcastService extends Service implements MediaPlayer.OnCompletionListener,
         MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener, MediaPlayer.OnSeekCompleteListener,
@@ -18,6 +32,7 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
 
     private static MediaPlayer pp;
     private static Episode episode = new Episode("","49731298537982742984232","","","","","");
+    private static Bitmap bitmap;
 
     //Used to pause/resume MediaPlayer
     private int resumePosition;
@@ -25,6 +40,20 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
     private AudioManager audioManager;
 
     private boolean shouldStart = true;
+
+    public static final String ACTION_PLAY = "com.campbellyamane.podantic.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "com.campbellyamane.podantic.ACTION_PAUSE";
+    public static final String ACTION_PREVIOUS = "com.campbellyamane.podantic.ACTION_PREVIOUS";
+    public static final String ACTION_NEXT = "com.campbellyamane.podantic.ACTION_NEXT";
+    public static final String ACTION_STOP = "com.campbellyamane.podantic.ACTION_STOP";
+
+    //MediaSession
+    private MediaSessionManager mediaSessionManager;
+    private MediaSession mediaSession;
+    private MediaController.TransportControls transportControls;
+
+    //AudioPlayer notification ID
+    private static final int NOTIFICATION_ID = 101;
 
     @Override
     public void onAudioFocusChange(int focusState) {
@@ -155,11 +184,13 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
     public void pauseMedia() {
         if (pp.isPlaying()) {
             pp.pause();
+            buildNotification(PlaybackStatus.PAUSED);
             resumePosition = pp.getCurrentPosition();
             shouldStart = false;
         }
         else if (!pp.isPlaying()){
             pp.start();
+            buildNotification(PlaybackStatus.PLAYING);
         }
     }
 
@@ -193,6 +224,7 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
     public void seekTo(int ms){
         pp.seekTo(ms);
     }
+
     @Override
     public void onCompletion(MediaPlayer mp) {
         //Invoked when playback of a media source has completed.
@@ -222,6 +254,21 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
     @Override
     public void onPrepared(MediaPlayer mp) {
         //Invoked when the media source is ready for playback.
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try  {
+                    URL url = new URL(episode.getArt());
+                    bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
+        buildNotification(PlaybackStatus.PLAYING);
         pp = mp;
         pp.start();
     }
@@ -247,8 +294,182 @@ public class PodcastService extends Service implements MediaPlayer.OnCompletionL
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         initMediaPlayer();
-
+        try {
+            initMediaSession();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        handleIncomingActions(intent);
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void initMediaSession() throws RemoteException {
+        if (mediaSessionManager != null) return; //mediaSessionManager exists
+
+        mediaSessionManager = (MediaSessionManager) getSystemService(Context.MEDIA_SESSION_SERVICE);
+        // Create a new MediaSession
+        mediaSession = new MediaSession(getApplicationContext(), "PodcastPlayer");
+        //Get MediaSessions transport controls
+        transportControls = mediaSession.getController().getTransportControls();
+        //set MediaSession -> ready to receive media commands
+        mediaSession.setActive(true);
+        //indicate that the MediaSession handles transport control commands
+        // through its MediaSessionCompat.Callback.
+        mediaSession.setFlags(MediaSession.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        //Set mediaSession's MetaData
+        updateMetaData();
+
+        // Attach Callback to receive MediaSession updates
+        mediaSession.setCallback(new MediaSession.Callback() {
+            // Implement callbacks
+            @Override
+            public void onPlay() {
+                super.onPlay();
+                resumeMedia();
+                buildNotification(PlaybackStatus.PLAYING);
+            }
+
+            @Override
+            public void onPause() {
+                super.onPause();
+                pauseMedia();
+                buildNotification(PlaybackStatus.PAUSED);
+            }
+
+            @Override
+            public void onSkipToNext() {
+                super.onSkipToNext();
+                //skipToNext();
+                updateMetaData();
+                buildNotification(PlaybackStatus.PLAYING);
+            }
+
+            @Override
+            public void onSkipToPrevious() {
+                super.onSkipToPrevious();
+                //skipToPrevious();
+                updateMetaData();
+                buildNotification(PlaybackStatus.PLAYING);
+            }
+
+            @Override
+            public void onStop() {
+                super.onStop();
+                removeNotification();
+                //Stop the service
+                stopSelf();
+            }
+
+            @Override
+            public void onSeekTo(long position) {
+                super.onSeekTo(position);
+            }
+        });
+    }
+
+    public enum PlaybackStatus {
+        PLAYING,
+        PAUSED
+    }
+
+    private void buildNotification(PlaybackStatus playbackStatus) {
+
+        int notificationAction = android.R.drawable.ic_media_pause;//needs to be initialized
+        PendingIntent play_pauseAction = null;
+
+        //Build a new notification according to the current state of the MediaPlayer
+        if (playbackStatus == PlaybackStatus.PLAYING) {
+            notificationAction = android.R.drawable.ic_media_pause;
+            //create the pause action
+            play_pauseAction = playbackAction(1);
+        } else if (playbackStatus == PlaybackStatus.PAUSED) {
+            notificationAction = android.R.drawable.ic_media_play;
+            //create the play action
+            play_pauseAction = playbackAction(0);
+        }
+
+        // Create a new Notification
+        Notification.Builder notificationBuilder = (Notification.Builder) new Notification.Builder(this)
+                .setShowWhen(false)
+                // Set the Notification style
+                .setStyle(new Notification.MediaStyle()
+                        // Attach our MediaSession token
+                        .setMediaSession(mediaSession.getSessionToken())
+                        // Show our playback controls in the compact notification view.
+                        .setShowActionsInCompactView(0, 1, 2))
+                // Set the Notification color
+                .setColor(getResources().getColor(R.color.colorPrimary))
+                // Set the large and small icons
+                .setLargeIcon(bitmap)
+                .setSmallIcon(android.R.drawable.stat_sys_headset)
+                // Set Notification content information
+                .setContentText(episode.getPodcast())
+                .setContentTitle(episode.getTitle())
+                .setContentInfo(episode.getDate())
+                // Add playback actions
+                .addAction(android.R.drawable.ic_media_previous, "previous", playbackAction(3))
+                .addAction(notificationAction, "pause", play_pauseAction)
+                .addAction(android.R.drawable.ic_media_next, "next", playbackAction(2));
+
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
+
+    private void removeNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    private PendingIntent playbackAction(int actionNumber) {
+        Intent playbackAction = new Intent(this, PodcastService.class);
+        switch (actionNumber) {
+            case 0:
+                // Play
+                playbackAction.setAction(ACTION_PLAY);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 1:
+                // Pause
+                playbackAction.setAction(ACTION_PAUSE);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 2:
+                // Next track
+                playbackAction.setAction(ACTION_NEXT);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            case 3:
+                // Previous track
+                playbackAction.setAction(ACTION_PREVIOUS);
+                return PendingIntent.getService(this, actionNumber, playbackAction, 0);
+            default:
+                break;
+        }
+        return null;
+    }
+
+    private void handleIncomingActions(Intent playbackAction) {
+        if (playbackAction == null || playbackAction.getAction() == null) return;
+
+        String actionString = playbackAction.getAction();
+        if (actionString.equalsIgnoreCase(ACTION_PLAY)) {
+            transportControls.play();
+        } else if (actionString.equalsIgnoreCase(ACTION_PAUSE)) {
+            transportControls.pause();
+        } else if (actionString.equalsIgnoreCase(ACTION_NEXT)) {
+            transportControls.skipToNext();
+        } else if (actionString.equalsIgnoreCase(ACTION_PREVIOUS)) {
+            transportControls.skipToPrevious();
+        } else if (actionString.equalsIgnoreCase(ACTION_STOP)) {
+            transportControls.stop();
+        }
+    }
+
+    private void updateMetaData() {
+        // Update the current metadata
+        mediaSession.setMetadata(new MediaMetadata.Builder()
+                .putBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART, bitmap)
+                .putString(MediaMetadata.METADATA_KEY_ARTIST, episode.getPodcast())
+                .putString(MediaMetadata.METADATA_KEY_ALBUM, episode.getDate())
+                .putString(MediaMetadata.METADATA_KEY_TITLE, episode.getTitle())
+                .build());
     }
 
     @Override
